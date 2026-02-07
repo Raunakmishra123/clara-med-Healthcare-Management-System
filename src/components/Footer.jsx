b6c442b16a8d64e87a803d3defc42c005a3ade1e
raunakmishra123 <raunakmishramth20@gmail.com>
2026-02-07 04:14:20 +0000

    Updated footer phone number to standard international format

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+91-8402923885</li>
+            <li>+91 8402923885</li>
             <li>raunakmishramth20@gmail.com</li>
           </ul>
         </div>
