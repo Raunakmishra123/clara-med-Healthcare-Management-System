674aa520465a2848938dedee10c2d0010341b847
raunakmishra123 <raunakmishramth20@gmail.com>
2026-02-12 06:43:42 +0000

    Updated footer wrapper margin for consistent spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='md:mx-10'>
+    <div className='md:mx-8'>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
 
         <div>
