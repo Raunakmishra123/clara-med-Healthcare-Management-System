f215c1e90577b3738860fe7852791262e90f18d0
raunakmishra123 <raunakmishramth20@gmail.com>
2026-02-18 04:58:21 +0000

    Fixed typo in Contact page — Carrers changed to Careers

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
           <p className=' text-gray-500'>IIIT Bhagalpur <br /> Sabour bhagalpur</p>
           <p className=' text-gray-500'>Tel: +91 8402923885 <br /> Email: raunakmishramth20@gmail.com</p>
-          <p className=' font-semibold text-lg text-gray-600'>Carrers at Claramed</p>
+          <p className=' font-semibold text-lg text-gray-600'>Careers at Claramed</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
